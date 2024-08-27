95797ff1b5ad7f1167a04e44088a030f1fce83e2
Guptilious <42465910+Guptilious@users.noreply.github.com>
2024-08-27 18:49:16 +0100

    Update commands.js
    updated commands to pull the keys from viera_keys.js as the default ones in the panasonic viera node project don't have any of the apps listed.

--- commands.js
+++ commands.js
@@ -1,34 +1,44 @@
 #!/usr/bin/env node
 
-const {VieraKeys, Viera} = require('viera_connection/viera');
+const {Viera} = require('node-panasonic-viera');
+const {VieraKeys} = require('./viera-keys');
+
 const viera = new Viera();
 
+const fs = require('fs');
+
+const rawData = fs.readFileSync('tv_credentials.json');
+const credentials = JSON.parse(rawData);
+
+const appId = credentials.app_id;
+const encryptionKey = credentials.encryption_key;
+
 const validApps = ['netflix', 'youtube', 'disney', 'prime'];
-const appId = '';
-const encryptionKey = '';
+let tvIp;
 
 function watchPost(body) {
 
-	const tvCommand = body.command;
-	const tvIp = body.tvip;
+        const tvCommand = body.command;
+        const tvIp = body.tvip;
+        const isAppCommand = validApps.includes(tvCommand);
+        const isKeyCommand = tvCommand in VieraKeys;
 
-	if (tvCommand in VieraKeys) {
+        if (isAppCommand || isKeyCommand) {
 
-		const isAppCommand = validApps.includes(tvCommand);
-		const commandType = isAppCommand ? 'sendAppCommand' : 'sendKey';
+                const commandType = isAppCommand ? 'sendAppCommand' : 'sendKey';
+                const commandValue = isAppCommand ? tvCommand : VieraKeys[tvCommand];
+                console.log(commandType + "\n" + commandValue);
+                viera.connect(tvIp, appId, encryptionKey)
+                .then(() => {
+                        return viera[commandType](VieraKeys[tvCommand]);
+                })
+                .catch((error) => {
+                        console.log(error);
+                });
 
-		viera.connect(tvIp, appId, encryptionKey)
-		.then(() => {
-			return viera[commandType](VieraKeys[tvCommand]);
-		})
-		.catch((error) => {
-			console.log(error);
-		});
-
-	} else console.log("Command: " + tvCommand + " not found");
+        } else console.log("Command: " + tvCommand + " not found");
 }
 
 module.exports = {
-	watchPost,
+        watchPost,
 };
-
